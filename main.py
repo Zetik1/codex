adc06411ec3b8332ba7909672193422983cf9d6c
Zaki <shadowzvlad@gmail.com>
2026-08-28 06:05:49 +0300

    refactor: перенес вызов init_db() и ответ "ok" внутрь main()

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from app.db import init_db
 def main() -> None:
     print(f"Python {sys.version}")
 
-init_db()
-print("ok")
-
 if __name__ == "__main__":
-    main()
+    main(
+        init_db(),
+        print("ok")
+    )
